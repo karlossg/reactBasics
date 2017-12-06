574c68edb1423a9841212b6b8d60834c71d0d385
Karol <karol.gratus@gmail.com>
2017-12-06 15:59:14 +0100

    repairing branch

--- script.js
+++ script.js
@@ -1,22 +1,2 @@
-const Counter = React.createClass({
-    getInitialState: function() {
-        return {
-            counter: 0
-        };
-    },
-
-    increment: function() {
-        this.setState({
-            counter: this.state.counter + 1
-        });
-    },
-
-    render: function() {
-        return React.createElement('div', {onClick: this.increment},
-            React.createElement('span', {className: 'counter'}, 'Licznik ' + this.state.counter)
-        );
-    }
-});
-
-const element = React.createElement(Counter);
-ReactDOM.render(element, document.getElementById('app'));
+const app = React.createElement(App);
+ReactDOM.render(app, document.getElementById('app'));
