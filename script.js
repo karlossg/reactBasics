1e148ed27a55659de39484fa2a66586480fe3f49
Karol <karol.gratus@gmail.com>
2017-12-06 12:32:35 +0100

    firt component

--- script.js
+++ script.js
@@ -1,63 +1,26 @@
-// const element1 = React.createElement('div', {}, 'Hello world!');
-// ReactDOM.render(element, document.getElementById('app'));
+const image = {
+  name: 'Kotek',
+  src: 'http://imgur.com/n8OYCzR.png'
+};
 
-// const element2 =
-// React.createElement('div', {},
-//   React.createElement('h1', {}, 'Lista filmów'),
-//   React.createElement('ul', {},
-//     React.createElement('li', {},
-//       React.createElement('h2', {}, 'Harry Potter'),
-//       React.createElement('p', {}, 'Film o czarodzieju')
-//     ),
-//     React.createElement('li', {},
-//       React.createElement('h2', {}, 'Król Lew'),
-//       React.createElement('p', {}, 'Film opowiadający historię króla sawanny')
-//     )
-//   )
-// );
-
-// ReactDOM.render(element2, document.getElementById('app'));
-
-
-const movies = [
-  {
-    id: 1,
-    title: 'Harry Potter',
-    desc: 'film o czarodzieju',
-    poster: 'potter.jpg'
+const GalleryItem = React.createClass({
+  propTypes: {
+    image: React.PropTypes.object.isRequired,
   },
-  {
-    id: 2,
-    title: 'Król Lew',
-    desc: 'Film o królu sawanny',
-    poster: 'lion-king.jpg'
-  },
-  {
-    id: 3,
-    title: 'Władca Pierścieni',
-    desc: 'Film o hobbitach',
-    poster: 'lotr.jpg'
-  },
-  {
-    id: 4,
-    title: 'Hulk',
-    desc: 'Film o zielonej bestii',
-    poster: 'hulk.jpg'
-  }
-];
 
-const moviesElements = movies.map((movie) => 
-  React.createElement('li', {key: movie.id},
-    React.createElement('h2', {}, movie.title),
-    React.createElement('p', {}, movie.desc),
-    React.createElement('img',{src: movie.poster})
-  )
-);
+  render: function() {
+    return (
+      React.createElement('div', {},
+        React.createElement('h2', {}, this.props.image.name),
+        React.createElement('img', {src: this.props.image.src})
+      )
+    )
+  },
+  
+});
 
-const element =
-  React.createElement('div', {},
-    React.createElement('h1', {}, 'Lista filmów'),
-    React.createElement('ul', {}, moviesElements)
-  );
+
+
+const element = React.createElement(GalleryItem, {image: image});
 
 ReactDOM.render(element, document.getElementById('app'));
