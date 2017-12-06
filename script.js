c0aa8b775a1a6beba5b8e1e05931b55c8f82af10
karlossg <karol.gratus@gmail.com>
2017-12-06 22:21:11 +0100

    style

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-var Counter = React.createClass({
+const Counter = React.createClass({
     getInitialState: function() {
         return {
             counter: 0
         };
     },
 
+    increment: function() {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    },
+
     render: function() {
-        return React.createElement('div', {},
-            React.createElement('span', {}, 'Licznik ' + this.state.counter)
+        return React.createElement('div', {onClick: this.increment},
+            React.createElement('span', {className: 'counter'}, 'Licznik ' + this.state.counter)
         );
     }
 });
 
-var element = React.createElement(Counter);
+const element = React.createElement(Counter);
 ReactDOM.render(element, document.getElementById('app'));
