ecea5f664cb66d776ecf04a4aa8a0f7009f26737
Karol <karol.gratus@gmail.com>
2017-12-07 11:18:11 +0100

    done for now

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
-const IncrementButton = React.createClass({
+const Counter1 = React.createClass({
+    getInitialState: function() {
+        return {
+            counter: 0
+        };
+    },
+   
+
+    increment: function() {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    },
+
+    decrement: function() {
+        this.setState({
+            counter: this.state.counter -1
+        });
+    },
+
+    ComponentWillMount() {
+        console.log(this.state.counter)
+    },
+    
     render: function() {
-        console.log(this.props.increment)
-      return React.createElement('button', {}, '+')
-      
+        return React.createElement('div', {},
+            React.createElement('span', {className: 'counter'}, 'Licznik ' + this.state.counter),
+            React.createElement('button', {onClick: this.increment}, '+'),
+            React.createElement('button', {onClick: this.decrement}, '-')
+           
+        );
+        console.log(IncrementButton)
     }
-})
+});
 
-
-const Counter = React.createClass({
+const Counter2 = React.createClass({
     getInitialState: function() {
         return {
             counter: 0
@@ -28,10 +54,53 @@ const Counter = React.createClass({
     render: function() {
         return React.createElement('div', {},
             React.createElement('span', {className: 'counter'}, 'Licznik ' + this.state.counter),
-            React.createElement(IncrementButton, {onClick: function() {this.increment}} )
+            React.createElement('button', {onClick: this.increment}, '+'),
+            React.createElement('button', {onClick: this.decrement}, '-')
+
         );
     }
 });
 
-const element = React.createElement(Counter);
+const Counter3 = React.createClass({
+    getInitialState: function() {
+        return {
+            counter: 0
+        };
+    },
+
+    increment: function() {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    },
+
+    decrement: function() {
+        this.setState({
+            counter: this.state.counter -1
+        });
+    },
+    render: function() {
+        return React.createElement('div', {},
+            React.createElement('span', {className: 'counter'}, 'Licznik ' + this.state.counter),
+            React.createElement('button', {onClick: this.increment}, '+'),
+            React.createElement('button', {onClick: this.decrement}, '-')
+
+        );
+    }
+});
+
+const CounterList = React.createClass({
+    
+    render: function() {
+      return (
+        React.createElement('ul', {}, 
+          React.createElement(Counter1, {}),
+          React.createElement(Counter2, {}),
+          React.createElement(Counter3, {})
+       )
+      )
+    }
+  })
+
+const element = React.createElement(CounterList);
 ReactDOM.render(element, document.getElementById('app'));
